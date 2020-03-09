1c4b025153b0cac3959cbf42487c2402666482e1
robrose02 <55984669+robrose02@users.noreply.github.com>
2020-03-09 15:42:41 +0000

    Part of animations

--- VaccineBoi/Assets/Scripts/BasicMovment.cs
+++ VaccineBoi/Assets/Scripts/BasicMovment.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class BasicMovment : MonoBehaviour
 {
+
+    public Animator animator;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal"), 0.0f, 0.0f);
+        transform.position = transform.position + horizontal * Time.deltaTime;
     }
 }
