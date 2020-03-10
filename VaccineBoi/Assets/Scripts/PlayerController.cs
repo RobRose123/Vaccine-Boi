6f95afdc55075d9799c268657526f1a7a54aadff
robrose02 <55984669+robrose02@users.noreply.github.com>
2020-03-10 15:38:45 +0000

    CrosshairNotComplete

--- VaccineBoi/Assets/Scripts/PlayerController.cs
+++ VaccineBoi/Assets/Scripts/PlayerController.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BasicMovment : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
 
     public Animator animator;
+    public GameObject crossHair;
+    public GameObject bulletPrefab;
+
 
 
     // Start is called before the first frame update
@@ -19,10 +22,30 @@ public class BasicMovment : MonoBehaviour
     {
         Vector3 movment = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f);
 
+        MoveCrossHair();
+
         animator.SetFloat("Horizontal", movment.x);
         animator.SetFloat("Vertical", movment.y);
         animator.SetFloat("Magnitude", movment.magnitude);
 
         transform.position = transform.position + movment * Time.deltaTime;
+    }
+
+    private void MoveCrossHair()
+    {
+        Vector3 aim = new Vector3(Input.GetAxis("AimHorizontal"), Input.GetAxis("AimVertical"), 0.0f);
+
+        if (aim.magnitude > 0.0f)
+        {
+            aim.Normalize();
+            aim *= 0.4f;
+            crossHair.transform.localPosition = aim;
+        }
+        else
+        {
+            crossHair.SetActive(false);
+        }
+
+
     }
 }
