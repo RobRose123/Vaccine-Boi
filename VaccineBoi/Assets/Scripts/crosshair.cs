41cd482dca786d6b9250a50bafe94e79bf28d9fe
robrose02 <55984669+robrose02@users.noreply.github.com>
2020-03-16 14:51:21 +0000

    WalkingFixed

---
+++ VaccineBoi/Assets/Scripts/crosshair.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class crosshair : MonoBehaviour
+{
+    public GameObject crosshairs;
+    private Vector3 target;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = transform.GetComponent<Camera>().ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, transform.position.z));
+        crosshairs.transform.position = new Vector2(target.x, target.y);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
